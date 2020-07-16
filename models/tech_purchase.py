f4302964dcec38ed2d2c60129fafb259242f4150
brahim ayad <brahim-ayad@hotmail.com>
2020-07-16 03:37:10 -0700

    save config setting field values

--- models/tech_purchase.py
+++ models/tech_purchase.py
@@ -4,12 +4,29 @@
 from odoo import fields, models, api
 
 
-class Tech_purchase(models.TransientModel):
+class  ResConfigSettings(models.TransientModel):
 
     _inherit = "res.config.settings"
 
     first_limit = fields.Monetary('Seuil 1')
     second_limit = fields.Monetary('Seuil 2')
+    limit_id = fields.Many2one('purchase.order', string="Seuil 1 ID")
+
+    @api.model
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        set_param = self.env['ir.config_parameter'].sudo().set_param
+        set_param('tech_purchase.first_limit', int(self.first_limit))
+        set_param('tech_purchase.second_limit', int(self.second_limit))
+
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        res['first_limit'] = int(get_param('tech_purchase.first_limit'))
+        res['second_limit'] = int(get_param('tech_purchase.second_limit'))
+        return res
 
 
 class Tech_purchase_oreder(models.Model):
@@ -27,6 +44,9 @@ class Tech_purchase_oreder(models.Model):
         ('done', 'Locked'),
         ('cancel', 'Cancelled')
     ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
+    limit = fields.One2many('res.config.settings','limit_id', string="Seuils")
+    first_limit_id = fields.Monetary(related='limit.first_limit')
+    second_limit_id = fields.Monetary(related='limit.second_limit')
 
     def action_verifie(self):
         for rec in self:
